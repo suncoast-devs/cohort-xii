c5ba329a525d1a0e42fe4abeb298a84dce782d5b
SheaParrott <alexandergotthis@gmail.com>
2018-10-30 12:03:50 -0400

    add one button team one working

--- week-02/day-2/scoreboard/public/main.js
+++ week-02/day-2/scoreboard/public/main.js
@@ -1,5 +1,28 @@
+let count = 0
+
+let addOnePointForTeamOne = () => {
+  console.log('Hurray, we clicked!')
+  count = count + 1
+  let ptagCount = document.querySelector('p')
+  console.log(ptagCount)
+
+  //make ptag content the value of count
+  ptagCount.textContent = count
+}
+
 const main = () => {
-  document.querySelector('h1').textContent += '?'
+  //find the ptag
+  let ptagCount = document.querySelector('p')
+  console.log(ptagCount)
+
+  //make ptag content the value of count
+  ptagCount.textContent = count
+
+  //find add 1 button for team 1
+  let addOneButton = document.querySelector('button.add-for-team-one')
+  console.log(addOneButton)
+  //when the add buttom for team 1 is clicked, increment count and update ptag
+  addOneButton.addEventListener('click', addOnePointForTeamOne)
 }
 
 document.addEventListener('DOMContentLoaded', main)

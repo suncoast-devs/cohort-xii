9bb2dd6e3a83c80ff97094957041ac9eb58c03bb
Gavin Stark <gavin@gstark.com>
2018-10-30 15:18:08 -0400

    Some refactoring to scoreboard

--- week-02/day-2/scoreboard/public/main.js
+++ week-02/day-2/scoreboard/public/main.js
@@ -3,89 +3,69 @@ let countForTeamTwo = 0
 
 let addOnePointForTeamOne = () => {
   count = count + 1
-  let ptagCount = document.querySelector('p.score-for-team-one')
 
   //make ptag content the value of count
-  ptagCount.textContent = count
+  document.querySelector('.score-for-team-one').textContent = count
 }
 
 let subtractOnePointFromTeamOne = () => {
   count = count - 1
-  let ptagCount = document.querySelector('p.score-for-team-one')
-
-  //make ptag content the value of count
-  ptagCount.textContent = count
+  document.querySelector('.score-for-team-one').textContent = count
 }
 
 let addOnePointForTeamTwo = () => {
   countForTeamTwo = countForTeamTwo + 1
-  let ptagCount = document.querySelector('p.score-for-team-two')
 
-  //make ptag content the value of count
-  ptagCount.textContent = countForTeamTwo
+  document.querySelector('.score-for-team-two').textContent = countForTeamTwo
 }
 
 let subtractOnePointFromTeamTwo = () => {
   countForTeamTwo = countForTeamTwo - 1
-  let ptagCount = document.querySelector('p.score-for-team-two')
 
-  //make ptag content the value of count
-  ptagCount.textContent = countForTeamTwo
+  document.querySelector('.score-for-team-two').textContent = countForTeamTwo
 }
 
 let updateTeamOneName = () => {
   // Find the team one name input element
-  let teamOneNameInput = document.querySelector('input#new-team-one-name')
+  let teamOneNameInput = document.querySelector('#new-team-one-name')
 
   // Read whatever is in that input
   let newName = teamOneNameInput.value
 
   // Change the team one name to that value
   // find that element, change it's textContent
-  let teamOneName = document.querySelector('h2.team-one-name')
+  let teamOneName = document.querySelector('.team-one .name')
   teamOneName.textContent = newName
 }
 
 const main = () => {
-  //find the ptag
-  let ptagCount = document.querySelector('p.score-for-team-one')
+  //find the class score-for-team-one and change it's text content to equal whatever is in the variable count
+  document.querySelector('.team-one .score').textContent = count
 
-  //make ptag content the value of count
-  ptagCount.textContent = count
-
-  let ptagForTeamTwo = document.querySelector('p.score-for-team-two')
-
-  ptagForTeamTwo.textContent = countForTeamTwo
+  document.querySelector('.team-two .score').textContent = countForTeamTwo
 
   //find add 1 button for team 1
-  let addOneButton = document.querySelector('button.add-for-team-one')
-
   //when the add buttom for team 1 is clicked, increment count and update ptag
-  addOneButton.addEventListener('click', addOnePointForTeamOne)
+  document
+    .querySelector('.team-one .add')
+    .addEventListener('click', addOnePointForTeamOne)
 
-  let subtractOneButton = document.querySelector(
-    'button.subtract-from-team-one'
-  )
-  subtractOneButton.addEventListener('click', subtractOnePointFromTeamOne)
+  document
+    .querySelector('.team-one .subtract')
+    .addEventListener('click', subtractOnePointFromTeamOne)
 
   //find add 1 button for team 2
-  let addOneButtonForTeamTwo = document.querySelector('button.add-for-team-two')
+  document
+    .querySelector('.team-two .add')
+    .addEventListener('click', addOnePointForTeamTwo)
 
-  //when the add buttom for team 2 is clicked, increment count and update ptag
-  addOneButtonForTeamTwo.addEventListener('click', addOnePointForTeamTwo)
+  document
+    .querySelector('.team-two .subtract')
+    .addEventListener('click', subtractOnePointFromTeamTwo)
 
-  let subtractOneButtonFromTeamTwo = document.querySelector(
-    'button.subtract-from-team-two'
-  )
-  subtractOneButtonFromTeamTwo.addEventListener(
-    'click',
-    subtractOnePointFromTeamTwo
-  )
-
-  let updateTeamOneNameButton = document.querySelector(
-    'button.update-team-one-name'
-  )
-  updateTeamOneNameButton.addEventListener('click', updateTeamOneName)
+  document
+    .querySelector('.update-team-one-name')
+    .addEventListener('click', updateTeamOneName)
 }
 
 document.addEventListener('DOMContentLoaded', main)

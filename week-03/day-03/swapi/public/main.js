807bbdb838a1ce1174162f32ede804837c7ceb70
Gavin Stark <gavin@gstark.com>
2018-11-08 10:44:23 -0500

    Adds page number navigation

--- week-03/day-03/swapi/public/main.js
+++ week-03/day-03/swapi/public/main.js
@@ -19,26 +19,54 @@ class Character {
   }
 }
 
+let page = 1
+
+const nextPage = () => {
+  page += 1
+
+  let ul = document.querySelector('ul')
+  ul.innerHTML = ''
+
+  let newLI = document.createElement('li')
+  newLI.textContent = 'Loading...'
+  ul.appendChild(newLI)
+
+  fetch(`https://swapi.co/api/people/?page=${page}`)
+    .then(response => response.json())
+    .then(rawData => {
+      render(rawData)
+    })
+}
+
+const render = rawData => {
+  document.querySelector('h3').textContent = `Page ${page}`
+
+  let ul = document.querySelector('ul')
+  ul.innerHTML = ''
+
+  let characters = rawData.results.map(rawCharacterData => {
+    let newCharacter = new Character(rawCharacterData)
+
+    return newCharacter
+  })
+
+  characters.forEach(character => {
+    let newLI = document.createElement('li')
+    newLI.textContent = character.description()
+
+    ul.appendChild(newLI)
+  })
+}
+
 const main = () => {
   //
 
-  fetch('https://swapi.co/api/people/?page=4')
+  document.querySelector('button').addEventListener('click', nextPage)
+
+  fetch(`https://swapi.co/api/people/?page=${page}`)
     .then(response => response.json())
     .then(rawData => {
-      let characters = rawData.results.map(rawCharacterData => {
-        let newCharacter = new Character(rawCharacterData)
-
-        return newCharacter
-      })
-
-      characters.forEach(character => {
-        let ul = document.querySelector('ul')
-
-        let newLI = document.createElement('li')
-        newLI.textContent = character.description()
-
-        ul.appendChild(newLI)
-      })
+      render(rawData)
     })
 }
 

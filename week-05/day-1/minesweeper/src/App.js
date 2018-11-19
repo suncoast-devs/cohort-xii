dbd68f201dc025c6369ec4206d7a8aa3167e6fe3
Gavin Stark <gavin@gstark.com>
2018-11-19 09:27:33 -0500

    Initial HTML for Minesweeper

---
+++ week-05/day-1/minesweeper/src/App.js
@@ -0,0 +1,117 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <table>
+          <tbody>
+            <tr>
+              <td className="header" colSpan="8">
+                <select>
+                  <option value="0">Easy</option>
+                  <option value="1">Intermediate</option>
+                  <option value="2">Expert</option>
+                </select>
+                <button>😄</button>
+              </td>
+            </tr>
+            <tr>
+              <td className="header not-playing" colSpan="8">
+                Start a new game!
+              </td>
+            </tr>
+            <tr>
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+            </tr>
+            <tr>
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+            </tr>
+            <tr>
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+            </tr>
+            <tr>
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+            </tr>
+            <tr>
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+            </tr>
+            <tr>
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+            </tr>
+            <tr>
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+            </tr>
+            <tr>
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+              <td />
+            </tr>
+            <tr>
+              <td className="header" colSpan="8">
+                10 mines left
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
+
+export default App

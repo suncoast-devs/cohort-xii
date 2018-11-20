27692ab052a9f7fa4fcd0029081770339f02f85e
Gavin Stark <gavin@gstark.com>
2018-11-20 10:26:55 -0500

    Moved state to the app, now have to pass values and functions to the counter

--- week-05/day-2/mobx/src/App.js
+++ week-05/day-2/mobx/src/App.js
@@ -4,10 +4,44 @@ import './App.css'
 import Counter from './Counter'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      value: 0
+    }
+  }
+
+  increment = event => {
+    this.setState({
+      value: this.state.value + 1
+    })
+  }
+
+  decrement = event => {
+    this.setState({
+      value: this.state.value - 1
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <Counter />
+        <Counter
+          increment={this.increment}
+          decrement={this.decrement}
+          value={this.state.value}
+        />
+        <Counter
+          increment={this.increment}
+          decrement={this.decrement}
+          value={this.state.value}
+        />
+        <Counter
+          increment={this.increment}
+          decrement={this.decrement}
+          value={this.state.value}
+        />
       </div>
     )
   }
--- week-05/day-2/mobx/src/Counter.js
+++ week-05/day-2/mobx/src/Counter.js
@@ -1,32 +1,12 @@
 import React, { Component } from 'react'
 
 class Counter extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      value: 0
-    }
-  }
-
-  increment = event => {
-    this.setState({
-      value: this.state.value + 1
-    })
-  }
-
-  decrement = event => {
-    this.setState({
-      value: this.state.value - 1
-    })
-  }
-
   render() {
     return (
       <div>
-        <p>{this.state.value}</p>
-        <button onClick={this.increment}>Increment</button>
-        <button onClick={this.decrement}>Decrement</button>
+        <p>{this.props.value}</p>
+        <button onClick={this.props.increment}>Increment</button>
+        <button onClick={this.props.decrement}>Decrement</button>
       </div>
     )
   }

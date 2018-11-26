e0747c90b98bb454338976f87286ea2ec876a21a
Gavin Stark <gavin@gstark.com>
2018-11-26 11:25:26 -0500

    Static markup, making an empty board state

--- week-06/react-tic-tac-toe/src/App.js
+++ week-06/react-tic-tac-toe/src/App.js
@@ -3,6 +3,14 @@ import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      board: ['', '', '', '', '', '', '', '', '']
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +18,9 @@ class App extends Component {
         <h2 />
         <div class="board">
           <div class="row">
+            <div>X</div>
             <div />
-            <div />
-            <div />
+            <div>O</div>
           </div>
           <div class="row">
             <div />

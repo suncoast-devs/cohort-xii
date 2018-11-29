842a95348f1c78a203bf21fab73c8cc455e8f42c
Gavin Stark <gavin@gstark.com>
2018-11-29 10:42:02 -0500

    Basic navigation and browser history

--- week-06/day-3/react-router-demo/src/About.js
+++ week-06/day-3/react-router-demo/src/About.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 class About extends Component {
   render() {
-    return <div>HOME PAGE!</div>
+    return <p>ABOUT PAGE!</p>
   }
 }
 
--- week-06/day-3/react-router-demo/src/App.js
+++ week-06/day-3/react-router-demo/src/App.js
@@ -2,12 +2,57 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import Home from './Home'
+import About from './About'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      route: window.location.pathname
+    }
+  }
+
+  page = () => {
+    if (this.state.route === '/home') {
+      return <Home />
+    }
+
+    if (this.state.route === '/about') {
+      return <About />
+    }
+  }
+
+  goHome = event => {
+    event.preventDefault()
+
+    this.setState({
+      route: '/home'
+    })
+
+    window.history.pushState(undefined, 'Home', '/home')
+  }
+
+  goAbout = event => {
+    event.preventDefault()
+
+    this.setState({
+      route: '/about'
+    })
+
+    window.history.pushState(undefined, 'About', '/about')
+  }
+
   render() {
     return (
       <div className="App">
-        <Home />
+        <a href="#" onClick={this.goHome}>
+          Go to Home
+        </a>
+        <a href="#" onClick={this.goAbout}>
+          Go to About
+        </a>
+        {this.page()}
       </div>
     )
   }

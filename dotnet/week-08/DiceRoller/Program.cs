e46feb7c423e02910d4f61e17ce3e13710b3a740
Mark Dewey <mark.t.dewey@gmail.com>
2018-12-10 11:20:44 -0500

    start of nice roller

---
+++ dotnet/week-08/DiceRoller/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiceRoller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, let us roll some dice!");
+        }
+    }
+}

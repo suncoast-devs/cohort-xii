653f1d1945808b72a6fd7ef067a759bc4a58c4c0
Mark Dewey <mark.t.dewey@gmail.com>
2018-12-17 11:18:12 -0500

    added CRUD operstions

--- dotnet/week-09/IntroToOrms/Program.cs
+++ dotnet/week-09/IntroToOrms/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace IntroToOrms
 {
@@ -30,11 +31,64 @@ namespace IntroToOrms
             //     MaxIso = iso
             // });
 
-            db.SaveChanges();
+            // db.SaveChanges();
 
-            // READ data 
+            // READ data
+            var allCameras = db.Cameras;
+
+            foreach (var camera in allCameras)
+            {
+                System.Console.WriteLine($"{camera.Brand} has an ISO of {camera.MaxIso}");
+            }
+            // WHERE
+            var onlyFullFrames = db.Cameras.Where(camera => camera.IsFullFrame);
+            System.Console.WriteLine("My Full Frame Cameras");
+            foreach (var camera in onlyFullFrames)
+            {
+                System.Console.WriteLine($"{camera.Brand} has an ISO of {camera.MaxIso}");
+            }
+
+            // SELECT FramesPerSecond FROM Cameras
+            var myFPS = db.Cameras.Select(camera => camera.FramesPerSecond);
+            System.Console.WriteLine("My Frames per seconds for all cameras");
+            foreach (var fps in myFPS)
+            {
+                System.Console.WriteLine(fps);
+            }
+
+            // SELECT FramesPerSecond , Brand, Id FROM Cameras
+            var results = db.Cameras.Select(camera => new { camera.FramesPerSecond, camera.Brand, camera.Id });
+            System.Console.WriteLine("My results");
+            foreach (var camera in results)
+            {
+                System.Console.WriteLine($"{camera.Brand} has {camera.FramesPerSecond}, and an Id of {camera.Id} ");
+            }
             // UPDATE 
+            // UPDATE Cameras SET MaxIso=102400 WHERE Id = 2
+            // part 1: find the camera, pull into context
+            var cannon1dx = db.Cameras.FirstOrDefault(camera => camera.Id == 2);
+            if (cannon1dx != null)
+            {
+                // part 2: update the data 
+                cannon1dx.MaxIso = 102400;
+                // part 3: Save the changes
+                db.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("Camera with ID 2 not found");
+            }
             // DELETE
+            // part 1: find it 
+            var toRemove = db.Cameras.FirstOrDefault(camera => camera.Id == 2);
+            // part 2: delete it 
+            if (toRemove != null)
+            {
+                db.Cameras.Remove(toRemove);
+                // part 3 : save your changes
+                db.SaveChanges();
+            }
+
         }
     }
 }

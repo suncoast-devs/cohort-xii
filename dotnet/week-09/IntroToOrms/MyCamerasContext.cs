cf6dc5de63cdc28c3a57cbc3dafcb3959484a5e6
Mark Dewey <mark.t.dewey@gmail.com>
2018-12-17 10:29:59 -0500

    added db context

---
+++ dotnet/week-09/IntroToOrms/MyCamerasContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace IntroToOrms
+{
+    public partial class MyCamerasContext : DbContext
+    {
+        public MyCamerasContext()
+        {
+        }
+
+        public MyCamerasContext(DbContextOptions<MyCamerasContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("server=localhost;database=MyCameras");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.0-rtm-35687");
+        }
+    }
+}

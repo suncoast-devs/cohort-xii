bc0cdd05fa89c2afc7041f5247ebbabdb2af6d79
Mark Dewey <mark.t.dewey@gmail.com>
2018-12-17 10:56:30 -0500

    added create statement

--- dotnet/week-09/IntroToOrms/Program.cs
+++ dotnet/week-09/IntroToOrms/Program.cs
@@ -6,7 +6,35 @@ namespace IntroToOrms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var db = new MyCamerasContext();
+
+            // CREATE data
+            // db.Cameras.Add(new Cameras{
+            //     Brand ="Cannon 7d",
+            //     FramesPerSecond = 10,
+            //     IsFullFrame = false,
+            //     MaxIso = 51200 });
+
+            // db.SaveChanges();
+            // Get info from console and add to DB
+            // System.Console.WriteLine("Enter the camera details");
+            // var brand = Console.ReadLine();
+            // var fps = int.Parse(Console.ReadLine());
+            // var isFullFrame = bool.Parse(Console.ReadLine());
+            // var iso = int.Parse(Console.ReadLine());
+
+            // db.Cameras.Add(new Cameras{
+            //     Brand = brand,
+            //     FramesPerSecond = fps, 
+            //     IsFullFrame = isFullFrame,
+            //     MaxIso = iso
+            // });
+
+            db.SaveChanges();
+
+            // READ data 
+            // UPDATE 
+            // DELETE
         }
     }
 }

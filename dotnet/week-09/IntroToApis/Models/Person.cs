fd93de5bf534e4090eee17f8c283b3e971d2c238
Mark Dewey <mark.t.dewey@gmail.com>
2018-12-18 12:09:07 -0500

    added GET, POST and DELET

---
+++ dotnet/week-09/IntroToApis/Models/Person.cs
@@ -0,0 +1,10 @@
+namespace IntroToApis.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ dotnet/week-09/IntroToApis/PhoneBookContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using IntroToApis.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace IntroToApis
+{
+    public partial class PhoneBookContext : DbContext
+    {
+        public PhoneBookContext()
+        {
+        }
+
+        public PhoneBookContext(DbContextOptions<PhoneBookContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("server=localhost;database=PhoneBook");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.0-rtm-35687");
+        }
+
+        public DbSet<Person> Person { get; set; }
+    }
+}

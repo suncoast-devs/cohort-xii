88f62901808e4cb08861ec26995f970a87693b2c
Mark Dewey <mark.t.dewey@gmail.com>
2018-12-18 10:36:06 -0500

    baseline for API talk

---
+++ dotnet/week-09/SampleDatabase/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace SampleDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var db = new TreeDatabaseContext();
+
+            db.Trees.Add(new Trees
+            {
+                Name = "Pine",
+                AverageAge = 110,
+                AverageHeight = 125.4
+            });
+
+
+            var treeNames = db.Trees.Where(tree => tree.AverageAge > 100).Select(s => s.Name);
+
+            treeNames.ToList().ForEach(Console.WriteLine);
+
+
+        }
+    }
+}
---
+++ dotnet/week-09/SampleDatabase/TreeDatabaseContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace SampleDatabase
+{
+    public partial class TreeDatabaseContext : DbContext
+    {
+        public TreeDatabaseContext()
+        {
+        }
+
+        public TreeDatabaseContext(DbContextOptions<TreeDatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("server=localhost;database=TreeDatabase");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.0-rtm-35687");
+        }
+
+        // public DbSet<ModelClassName> TableName{get;set;}
+        public DbSet<Trees> Trees { get; set; }
+    }
+}

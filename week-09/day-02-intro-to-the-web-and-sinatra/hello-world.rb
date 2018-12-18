3c328fc471e8c3d5e1894be451f05f81d74d3b06
Gavin Stark <gavin@gstark.com>
2018-12-18 15:40:08 -0500

    Create, read, update, delete

--- week-09/day-02-intro-to-the-web-and-sinatra/hello-world.rb
+++ week-09/day-02-intro-to-the-web-and-sinatra/hello-world.rb
@@ -28,14 +28,15 @@ get '/joke' do
   json        joke: JOKES[number]
 end
 
+# Get all the jokes
 get '/jokes' do
-  all_jokes = Joke.all
+  all_jokes = Joke.all.order(:id)
 
-  json    jokes: all_jokes
+  json  all_jokes
 end
 
+# Get one joke
 get '/jokes/:id' do
-
   joke_from_database = Joke.find(params["id"])
 
   json   joke: joke_from_database
@@ -46,3 +47,51 @@ get '/jokes/:id' do
   #json joke: JOKES[id]
 end
 
+# Creating a joke
+post '/jokes' do
+  #
+  # our input body will look like this:
+  #
+  # {
+  #   joke: {
+  #     punchline: "joke here"
+  #   }
+  # }
+  #
+
+  # Gavin to research this, is there a better way?
+  data = JSON.parse(request.body.read)
+
+  joke_params = data["joke"]
+
+  # Example:
+  #   Joke.create(punchline: "joke here")
+  new_joke = Joke.create(joke_params)
+
+  json joke: new_joke
+end
+
+# Updating a joke
+put '/jokes/:id' do
+  # Gavin to research this, is there a better way?
+  data = JSON.parse(request.body.read)
+
+  joke_params = data["joke"]
+
+  # Find the joke by it's ID
+  existing_joke = Joke.find(params["id"])
+ 
+  # Update it with the new parameters from joke_params
+  existing_joke.update(joke_params)
+
+  json joke: existing_joke
+end
+
+# Delete a joke
+delete '/jokes/:id' do
+  deleting_joke = Joke.find(params["id"])
+
+  deleting_joke.destroy
+
+  json joke: deleting_joke
+end

2df70d6876bbbb95c84d90fb4723fbe2078d37cc
Mark Dewey <mark.t.dewey@gmail.com>
2018-12-20 11:59:40 -0500

    added react roouter

--- dotnet/week-09/jabberdexicon/jabberdexicon-client/src/App.jsx
+++ dotnet/week-09/jabberdexicon/jabberdexicon-client/src/App.jsx
@@ -1,57 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Axios from 'axios';
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from './Pages/Home';
+import Add from './Pages/Add';
 class App extends Component {
-
-  state = {
-    words: []
-  }
-
-  loadLetter = letter => {
-    console.log(letter)
-    Axios
-      .get(`https://localhost:5001/api/words/${letter}`)
-      .then(resp => {
-        console.log({ resp })
-        this.setState({
-          words: resp.data
-        })
-      })
-  }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Jabberdexicon
-          </p>
-        </header>
-        <main>
-          <section>
+      <Router>
+        <>
+          <div className="App">
+            <header className="App-header">
+              <p>
+                Jabberdexicon
+              </p>
+            </header>
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/add" exact component={Add} />
+            </Switch>
+          </div>
+        </>
+      </Router>
 
-            {"abcdefghijklmnopqrstuvwxyz".split('').map(letter => {
-              return (
-                <button key={letter} onClick={() => this.loadLetter(letter)}>{letter}</button>
-              )
-            })}
-          </section>
-          <section>
-            <header>words</header>
-            <ul>
-              {this.state.words.map(word => {
-                return (
-                  <li key={word.id}>{word.value}</li>
-                )
-              })}
-            </ul>
-          </section>
-
-        </main>
-      </div>
     );
   }
 }

8bf664d3e7f0d0a31dedbbfa01c78e0f46f948a0
Mark Dewey <mark.t.dewey@gmail.com>
2019-01-02 10:18:09 -0500

    ready to go with car api .net

--- dotnet/week-09/jabberdexicon/jabberdexicon-client/src/Pages/Add.jsx
+++ dotnet/week-09/jabberdexicon/jabberdexicon-client/src/Pages/Add.jsx
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 class Add extends Component {
+
+    updateWord = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    submitWord = e => {
+        e.preventDefault();
+        Axios
+            .post("https://localhost:5001/api/words", { word: this.state.newWord })
+            .then(resp => {
+                // add text that says it was successful
+            })
+    }
     render() {
         return (
             <div>
-                AddPage
+                <form onSubmit={this.submitWord}>
+                    <input type="text" onChange={this.updateWord} name="newWord" />
+                    <button>Add Word</button>
+                </form>
             </div>
         );
     }

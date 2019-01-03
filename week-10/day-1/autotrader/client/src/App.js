bf4db76c1aef48199c5bbeaaf455d5f763a450ae
Gavin Stark <gavin@gstark.com>
2019-01-03 11:15:22 -0500

    Using our live API

--- week-10/day-1/autotrader/client/src/App.js
+++ week-10/day-1/autotrader/client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class App extends Component {
   constructor(props) {
@@ -6,59 +7,25 @@ class App extends Component {
 
     this.state = {
       cars: [
-        {
-          "id": 1,
-          "zip": "33703",
-          "price": 1231,
-          "milage": 234234,
-          "body_style": "coupe",
-          "interior_color": "black",
-          "exterior_color": "white",
-          "model_name": "S",
-          "make": "Tesla Motors"
-        },
-        {
-          "id": 2,
-          "zip": "44444",
-          "price": 123123,
-          "milage": 4444,
-          "body_style": "couple",
-          "interior_color": "green",
-          "exterior_color": "yellow",
-          "model_name": "Golf",
-          "make": "Volkswagen"
-        },
-        {
-          "id": 3,
-          "zip": "4444",
-          "price": 444,
-          "milage": 444,
-          "body_style": "44",
-          "interior_color": "44",
-          "exterior_color": "44",
-          "model_name": "S",
-          "make": "Tesla Motors"
-        },
-        {
-          "id": 4,
-          "zip": "11111",
-          "price": 11111,
-          "milage": 11111,
-          "body_style": "couple",
-          "interior_color": "black",
-          "exterior_color": "black",
-          "model_name": "i3",
-          "make": "BMW"
-        }
       ]
     }
   }
+
+  componentDidMount() {
+    axios.get('/api/cars/index').then(response => {
+      console.log(response.data)
+      this.setState({
+        cars: response.data
+      })
+    })
+  }
+
   render() {
     return <>
       <h1>AutoTrader</h1>
       <ul>
         {this.state.cars.map(car => <li>
-          {car.make} {car.model} ${car.price}
+          {car.make} {car.model_name} ${car.price}
         </li>)}
       </ul>
     </>

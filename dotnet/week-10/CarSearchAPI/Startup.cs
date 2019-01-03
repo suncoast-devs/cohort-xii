3e87dde15d211ed9e08160ac69094a35d201332c
Mark Dewey <mark.t.dewey@gmail.com>
2019-01-03 15:42:54 -0500

    added health checks

--- dotnet/week-10/CarSearchAPI/Startup.cs
+++ dotnet/week-10/CarSearchAPI/Startup.cs
@@ -35,7 +35,8 @@ namespace carsearchapi
                 .AddJsonOptions(options =>
             {
                 options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
-            }); ;
+            });
+            services.AddHealthChecks();
             // Register the Swagger generator, defining 1 or more Swagger documents
             services.AddSwaggerGen(c =>
             {
@@ -56,7 +57,8 @@ namespace carsearchapi
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            
+            app.UseHealthChecks("/healthz");
             app.UseHttpsRedirection();
             app.UseCors(builder =>
               builder

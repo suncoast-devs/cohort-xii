f57b0189afb6d529e7c6d17031090b72369d8f91
Gavin Stark <gavin@gstark.com>
2019-01-03 11:06:28 -0500

    Sets up React app right inside our Rails application

--- week-10/day-1/autotrader/config/routes.rb
+++ week-10/day-1/autotrader/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     get 'cars/index'
   end
+
   namespace :admin do
     resources :cars
     resources :models
@@ -9,4 +10,9 @@ Rails.application.routes.draw do
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  if Rails.env.production?
+    CLIENT_HTML = File.read(Rails.root.join('public/index.html'))
+
+    get "*path", to: proc { [200, {}, [CLIENT_HTML]] }
+  end
 end

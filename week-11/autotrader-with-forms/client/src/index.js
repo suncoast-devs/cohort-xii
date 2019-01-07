7d86f1442429def58d239d01b186cf4cc1f5bece
Gavin Stark <gavin@gstark.com>
2019-01-07 13:33:41 -0500

    Adds missing dependencies so bootstrap javascript functions correctly

--- week-11/autotrader-with-forms/client/src/index.js
+++ week-11/autotrader-with-forms/client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
 
 ReactDOM.render(<App />, document.getElementById('root'))

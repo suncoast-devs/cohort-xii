76e355ad267f6663160a4843bf4f32fc70f54612
Gavin Stark <gavin@gstark.com>
2019-01-08 15:24:00 -0500

    Adds clicking of markers

--- week-11/tacotuesday/client/src/LocationMap.js
+++ week-11/tacotuesday/client/src/LocationMap.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
-import MapGL, { Marker, NavigationControl } from 'react-map-gl'
+import MapGL, { Marker, Popup, NavigationControl } from 'react-map-gl'
+import pin from './pin.png'
 
 class LocationMap extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      popupInfo: null,
       viewport: {
-        latitude: 37.785164,
-        longitude: -100,
-        zoom: 3.5,
+        latitude: 27.7700989,
+        longitude: -82.6364093,
+        zoom: 12.5,
         bearing: 0,
         pitch: 0
       }
@@ -17,6 +19,31 @@ class LocationMap extends Component {
 
   _updateViewport = newViewport => {
     this.setState({ viewport: newViewport })
+  }
+
+  renderPopup = () => {
+    const popupInfo = this.state.popupInfo
+
+    if (!popupInfo) {
+      return
+    }
+
+    return (
+      <Popup
+        tipSize={5}
+        anchor="top"
+        longitude={popupInfo.longitude}
+        latitude={popupInfo.latitude}
+        closeOnClick={false}
+        onClose={() => this.setState({ popupInfo: null })}
+      >
+        <div>
+          <p>{popupInfo.company_name}</p>
+          <p>{popupInfo.address}</p>
+          <p>{popupInfo.hours}</p>
+        </div>
+      </Popup>
+    )
   }
 
   render() {
@@ -42,16 +69,21 @@ class LocationMap extends Component {
           <NavigationControl onViewportChange={this._updateViewport} />
         </div>
 
+        {this.renderPopup()}
+
         {this.props.locations.map(location => {
           return (
             <Marker
               key={location.id}
               longitude={location.longitude}
               latitude={location.latitude}
+              offsetTop={-64}
+              offsetLeft={-32}
             >
               <img
+                onClick={() => this.setState({ popupInfo: location })}
                 width="64"
-                src="http://chittagongit.com//images/free-map-pin-icon/free-map-pin-icon-6.jpg"
+                src={pin}
               />
             </Marker>
           )

0e2bacd83bcfc7c6f2a5827a3ddc82a902593dca
Gavin Stark <gavin@gstark.com>
2019-01-08 11:04:31 -0500

    Adds geocoding to our models
    Now when we create a company or a location we get a latitude and longitude automatically

--- week-11/tacotuesday/app/models/company.rb
+++ week-11/tacotuesday/app/models/company.rb
@@ -1,3 +1,7 @@
 class Company < ApplicationRecord
   has_many :locations
+
+  geocoded_by :address
+
+  after_validation :geocode
 end
--- week-11/tacotuesday/db/schema.rb
+++ week-11/tacotuesday/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_08_153409) do
+ActiveRecord::Schema.define(version: 2019_01_08_155431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2019_01_08_153409) do
     t.integer "year_founded"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "latitude"
+    t.decimal "longitude"
   end
 
   create_table "locations", force: :cascade do |t|
@@ -30,6 +32,8 @@ ActiveRecord::Schema.define(version: 2019_01_08_153409) do
     t.bigint "company_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "latitude"
+    t.decimal "longitude"
     t.index ["company_id"], name: "index_locations_on_company_id"
   end
 
